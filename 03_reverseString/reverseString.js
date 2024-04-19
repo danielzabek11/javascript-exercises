b7a54e4aaa7b29a8b76749a2035266589fc29d69
Daniel Zabek <danielzabek11@gmail.com>
2024-04-19 19:24:23 +0200

    Finish 03 task

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
+const reverseString = function(stringToReverse) {
+    let wordsArray = stringToReverse.split("");
+    return wordsArray.reverse().join("");
 
 };
-
+reverseString('hello there') 
 // Do not edit below this line
 module.exports = reverseString;
