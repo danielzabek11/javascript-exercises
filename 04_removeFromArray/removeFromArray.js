d1e08f0a8ec478cd0c8cb1d1b9119d1c090c7d2c
Daniel Zabek <danielzabek11@gmail.com>
2024-04-19 19:54:55 +0200

    Finish 04 task

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...elementToRemove) {
+/*     newArr = [];
 
+    for (const element of arr) {
+        if(!elementToRemove.includes(element)) {
+            newArr.push(element);
+        }
+    }
+    return newArr; */
+    return arr.filter((element) => !elementToRemove.includes(element));
 };
-
+removeFromArray([1, 2, 3, 4], 3); 
 // Do not edit below this line
 module.exports = removeFromArray;
