8543a92504063d533c0bc9a7115d4ed0f366011d
Daniel Zabek <danielzabek11@gmail.com>
2024-04-19 20:55:17 +0200

    Finish 06 task

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if ((0 == year % 4) && (0 != year % 100) || (0 == year % 400)) {
+        return true;
+    }
+    return false;
 };
-
+leapYears(2000) 
 // Do not edit below this line
 module.exports = leapYears;
