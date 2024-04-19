a5913a60e21cba3e864bab9dbc7a8785ec5d1693
Daniel Zabek <danielzabek11@gmail.com>
2024-04-19 19:11:33 +0200

    Finish 02 task

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
+const repeatString = function(stringToMultiply, times) {
+    if (times < 0) {
+        return 'ERROR'
+    }
+    return stringToMultiply.repeat(times)
 
 };
-
+repeatString('hey', 3)
 // Do not edit below this line
 module.exports = repeatString;
