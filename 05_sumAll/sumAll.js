ba7a81bd02493d1c7ead478c6db0e121517ccd2f
Daniel Zabek <danielzabek11@gmail.com>
2024-04-19 20:14:22 +0200

    Finish 05 task

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if(start < 0 || end < 0 || typeof start !== "number" || typeof end !== "number") {
+        return 'ERROR'
+    }
 
+    if(start > end) {
+        [start, end] = [end, start];
+    }
+
+    let sum = 0;
+    for(i=start; i<=end; i++) {
+        sum += i
+    }
+    return sum
 };
-
+sumAll(1, 4)
 // Do not edit below this line
 module.exports = sumAll;
